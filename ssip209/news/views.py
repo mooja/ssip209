36c34598af0611e1eb6ca3f4ed330563c4ad7466
Max Shkurygin <max.atreides@gmail.com>
2014-09-22 22:34:53 -0400

    removed unneeded mixin

--- ssip209/news/views.py
+++ ssip209/news/views.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
-
 from django.views.generic import DetailView, ListView
-from braces.views import LoginRequiredMixin
 from news.models import NewsEntry
 
 # Create your views here.
 
-class NewsEntryDetailView(LoginRequiredMixin, DetailView):
+class NewsEntryDetailView(DetailView):
     model = NewsEntry
     context_object_name = "news_entry"
     template_name = "news/news_entry_detail.html"
