4976114756b3f0027d4a65238ec409dfa8fe0d89
Max Shkurygin <max.atreides@gmail.com>
2014-09-24 12:44:55 -0400

    Protecting some sensetive data for now.

--- ssip209/events/views.py
+++ ssip209/events/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from swingtime.views import *
 
 # Create your views here.
-view_view = (event_view)
-event_listing = (event_listing)
-occurrence_view = (occurrence_view)
-add_event = (add_event)
+view_view = login_required(event_view)
+event_listing = login_required(event_listing)
+occurrence_view = login_required(occurrence_view)
+add_event = login_required(add_event)
--- ssip209/members/views.py
+++ ssip209/members/views.py
@@ -1,14 +1,15 @@
 from django.views.generic import DetailView, ListView
+from braces.views import LoginRequiredMixin
 from members.models import Member
 
 # Create your views here.
-class MemberDetailView(DetailView):
+class MemberDetailView(LoginRequiredMixin, DetailView):
     model = Member
     context_object_name = "member"
     template_name = "members/member_detail.html"
 
 
-class MemberListView(ListView):
+class MemberListView(LoginRequiredMixin, ListView):
     model = Member
     context_object_name = "member_list"
     template_name = "members/members_list.html"
