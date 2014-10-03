176803b25694a20c1829eb2cd594c5adb115e1e3
Max Shkurygin <max.atreides@gmail.com>
2014-10-03 09:14:31 -0400

    removed things from the production config that i don't understand yet

--- source/config/production.py
+++ source/config/production.py
@@ -10,12 +10,12 @@ Production Configurations
 from configurations import values
 
 # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
-try:
-    from S3 import CallingFormat
-    AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
-except ImportError:
-    # TODO: Fix this where even if in Dev this class is called.
-    pass
+# try:
+#     from S3 import CallingFormat
+#     AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
+# except ImportError:
+#     # TODO: Fix this where even if in Dev this class is called.
+#     pass
 
 from .common import Common
 
@@ -35,17 +35,17 @@ class Production(Common):
     # END SECRET KEY
 
     # django-secure
-    INSTALLED_APPS += ("djangosecure", )
+    # INSTALLED_APPS += ("djangosecure", )
 
-    # set this to 60 seconds and then to 518400 when you can prove it works
-    SECURE_HSTS_SECONDS = 60
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
-    SECURE_FRAME_DENY = values.BooleanValue(True)
-    SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
-    SECURE_BROWSER_XSS_FILTER = values.BooleanValue(True)
-    SESSION_COOKIE_SECURE = values.BooleanValue(False)
-    SESSION_COOKIE_HTTPONLY = values.BooleanValue(True)
-    SECURE_SSL_REDIRECT = values.BooleanValue(True)
+    # # set this to 60 seconds and then to 518400 when you can prove it works
+    # SECURE_HSTS_SECONDS = 60
+    # SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
+    # SECURE_FRAME_DENY = values.BooleanValue(True)
+    # SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
+    # SECURE_BROWSER_XSS_FILTER = values.BooleanValue(True)
+    # SESSION_COOKIE_SECURE = values.BooleanValue(False)
+    # SESSION_COOKIE_HTTPONLY = values.BooleanValue(True)
+    # SECURE_SSL_REDIRECT = values.BooleanValue(True)
     # end django-secure
 
     # SITE CONFIGURATION
@@ -58,9 +58,9 @@ class Production(Common):
 
     # STORAGE CONFIGURATION
     # See: http://django-storages.readthedocs.org/en/latest/index.html
-    INSTALLED_APPS += (
-        'storages',
-    )
+    # INSTALLED_APPS += (
+    #     'storages',
+    # )
 
     # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
     # STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
