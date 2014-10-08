216a0c1deef664038aa02e2ae67c629600bbc516
Max Shkurygin <max.atreides@gmail.com>
2014-10-08 13:48:13 -0400

    Added more filters to the member list view.

--- source/static/js/project.js
+++ source/static/js/project.js
@@ -1,3 +1,19 @@
 /* Project specific Javascript goes here. */
 
+$(document).ready(function () {
+    hobbies = $(".hobbies");
+    canhelp = $(".canhelp");
 
+    hobbies_button = $("#hobbies-toggle");
+    canhelp_button = $("#canhelp-toggle");
+
+    hobbies_button.click(function () {
+        hobbies_button.toggleClass('btn-success');
+        hobbies.toggle();
+    });
+
+    canhelp_button.click(function () {
+        canhelp_button.toggleClass('btn-success');
+        canhelp.toggle();
+    });
+})
