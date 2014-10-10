12c2ae55ae6600ef1fa4aec2fb195bf0edc21c5a
Max Shkurygin <max.atreides@gmail.com>
2014-10-10 19:11:47 -0400

    Greatly impreved the member list view.

--- source/static/js/project.js
+++ source/static/js/project.js
@@ -1,19 +1,34 @@
 /* Project specific Javascript goes here. */
 
 $(document).ready(function () {
-    hobbies = $(".hobbies");
-    canhelp = $(".canhelp");
+    var hobbies = $(".hobbies");
+    var canhelp = $(".canhelp");
 
-    hobbies_button = $("#hobbies-toggle");
-    canhelp_button = $("#canhelp-toggle");
+    var hobbies_button = $("#hobbies-toggle");
+    var canhelp_button = $("#canhelp-toggle");
+
+    $container = $("#members");
+
+    $container.isotope({
+        layoutMode: "masonry",
+        itemSelector: ".member-data",
+    });
 
     hobbies_button.click(function () {
         hobbies_button.toggleClass('btn-success');
-        hobbies.toggle();
+        hobbies.toggleClass("hidden");
+        $container.isotope('layout');
     });
 
     canhelp_button.click(function () {
         canhelp_button.toggleClass('btn-success');
-        canhelp.toggle();
+        canhelp.toggleClass("hidden");
+        $container.isotope('layout');
     });
-})
+
+    $(".member-data").hover(function () {
+        $(this).toggleClass("bg-warning");
+        $(this).find(".detail-data").toggleClass("hidden");
+        $container.isotope('layout');
+    });
+});
