6fe18eef4ceb4ca3f390e7d0f3fa806d902ec81e
Max Shkurygin <max.atreides@gmail.com>
2014-10-13 15:23:14 -0400

    sidebar html is now rendered in a seperate function

--- source/views.py
+++ source/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render_to_response
+
 from datetime import datetime, timedelta
 from pytz import timezone
 
@@ -6,23 +8,31 @@ from news.models import NewsEntry
 from swingtime.models import Occurrence
 
 
+def render_sidebar_html(template="sidebar.html"):
+    """ returns sidebar's rendered html as string """
+
+    eastern_tz = timezone('US/Eastern')
+    now = datetime.now(tz=eastern_tz)
+
+    upper_bound = datetime.now(tz=eastern_tz) + timedelta(weeks=4)
+    lower_bound = datetime.now(tz=eastern_tz) - timedelta(days=0)
+    upcoming_occurrence_list = Occurrence.objects.all(
+            ).filter(start_time__gte=lower_bound
+            ).filter(start_time__lte=upper_bound
+            ).exclude(event__event_type__abbr='week',
+                      start_time__gte=(now + timedelta(weeks=1)))
+
+    context = dict()
+    context['upcoming_occurrence_list'] = upcoming_occurrence_list
+    context['now'] = now
+
+    return render_to_response(template, context).content
+
 class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
-        eastern_tz = timezone('US/Eastern')
-        now = datetime.now(tz=eastern_tz)
-
-        upper_bound = datetime.now(tz=eastern_tz) + timedelta(weeks=4)
-        lower_bound = datetime.now(tz=eastern_tz) - timedelta(days=0)
-        upcoming_occurrence_list = Occurrence.objects.all(
-                ).filter(start_time__gte=lower_bound
-                ).filter(start_time__lte=upper_bound
-                ).exclude(event__event_type__abbr='week',
-                          start_time__gte=(now + timedelta(weeks=1)))
-
         context = super(TemplateView, self).get_context_data(**kwargs)
         context['news_entry_list'] = NewsEntry.objects.all()
-        context['upcoming_occurrence_list'] = upcoming_occurrence_list
-        context['now'] = now
+        context['sidebar_html'] = render_sidebar_html()
 
         return context
